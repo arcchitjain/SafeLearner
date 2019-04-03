f03b757d82093395d6693cbd35b90e462d57aa2c
Arcchit Jain <arcchit.jain.2015@iitkalumni.org>
2019-04-03 12:06:25 +0200

    Removed Ragni Files from AKBC19

--- Data/Ragni16/processed/ragni_exp_v1.sh
+++
@@ -1,5 +0,0 @@
-time python ../probfoil.py ../Data/Ragni/ragni_v1.pl -t response_all/3 --log ../Logs/Ragni/rall_v1.log -v -v -v &
-time python ../probfoil.py ../Data/Ragni/ragni_v1.pl -t response_some/3 --log ../Logs/Ragni/rsome_v1.log -v -v -v &
-time python ../probfoil.py ../Data/Ragni/ragni_v1.pl -t response_no/3 --log ../Logs/Ragni/rno_v1.log -v -v -v &
-time python ../probfoil.py ../Data/Ragni/ragni_v1.pl -t response_somenot/3 --log ../Logs/Ragni/rsomenot_v1.log -v -v -v &
-time python ../probfoil.py ../Data/Ragni/ragni_v1.pl -t response_nvc/3 --log ../Logs/Ragni/rnvc_v1.log -v -v -v
--- Data/Ragni16/processed/ragni_exp_v3.sh
+++
@@ -1,5 +0,0 @@
-time python ../probfoil.py ../Data/Ragni/ragni_v3_all.pl --log ../Logs/Ragni/rall_v3.log -v -v -v &
-time python ../probfoil.py ../Data/Ragni/ragni_v3_some.pl --log ../Logs/Ragni/rsome_v3.log -v -v -v &
-time python ../probfoil.py ../Data/Ragni/ragni_v3_no.pl --log ../Logs/Ragni/rno_v3.log -v -v -v &
-time python ../probfoil.py ../Data/Ragni/ragni_v3_somenot.pl --log ../Logs/Ragni/rsomenot_v3.log -v -v -v &
-time python ../probfoil.py ../Data/Ragni/ragni_v3_nvc.pl --log ../Logs/Ragni/rnvc_v3.log -v -v -v &
